a96c7bdcb67a5fb40ad49c043c2300fafc4c0399
Alaa Ali <73523304+alaa-ali126@users.noreply.github.com>
2022-08-05 19:43:05 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,10 @@
+let phone = document.querySelector(".img");
+let container = document.querySelector(".container");
+
+function phones(src) {
+  phone.src = src;
+}
+
+function colors(col) {
+  container.style.background = col;
+}
